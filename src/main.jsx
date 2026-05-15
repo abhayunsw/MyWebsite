24055759877538e3ca5d4d5d6dcb5cb5feca440f
Abhay <abhayunsw@gmail.com>
2026-05-15 21:54:02 +1000

    fixed margin issues

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// Fix overflow and borders
+document.body.style.margin = '0'
+document.body.style.padding = '0'
+document.body.style.overflow = 'hidden'
+document.documentElement.style.overflow = 'hidden'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
